fa5dd1564ab86ec193a46e73135f9f63560eeaf7
nimrosh <118816418+nimrodkatzenell@users.noreply.github.com>
2023-05-03 19:04:45 +0300

    this is hw
    hw3_

---
+++ src/hw3_/MainTask1.java
@@ -0,0 +1,10 @@
+package hw3_;
+
+public class MainTask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nknim
+ *
+ */
+module hw3_ {
+}
